c2ff52dd51d63ff593e0e56ae3a8453c3065deac
= <=>
2024-09-17 18:54:03 -0400

    Initialize inventory with product objects

---
+++ customer_orders.js
@@ -0,0 +1,9 @@
+// Task 1: Create an Inventory Array of Product Objects
+
+const inventory = [
+    { name: 'Chocolate', price: 5, quantity: 124},
+    { name: 'Lolipop', price: 1, quantity: 56},
+    { name: 'Candy', price: 2, quantity: 88 },
+    { name: 'Cookies', price: 6, quantity: 211 },
+];
+

6fb09e5c1e01bd971af220a3a04996693e329979
= <=>
2024-09-22 18:59:25 -0400

    Create placeOrder function

--- customer_orders.js
+++ customer_orders.js
@@ -10,24 +10,30 @@ const inventory = [
 // Tak 2: Create an Orders Array of Order Objects
 
 const orders = [];
-orders.push({
-    customerName: "Isabela",
-    orderedItems: ["Chocolate", "Candy"] ,
-    status: 'Pending'  
-  });
 
 // Task 3: Create a Function to Place an Order
 
 function placeOrder (customerName, orderedItems) {
     for (let item of orderedItems) {
-    const inventoryItem = inventory.find(element => element.name === item);
-   
-    if (inventoryItem === undefined) {
-    console.log(`Error: Product '${item}' not found in inventory.`)};
-    
-    else if { inventoryItem.quantity -
+        const inventoryItem = inventory.find(element => element.name === item.name);
 
-    }}
-};
+     if (inventoryItem === undefined) {
+     console.log(`Error: Product '${item.name}' not found in inventory.`);
+    return; 
+        
+} else if (inventoryItem.quantity < item.quantity) {
+        console.log(`Error:'${item.name} is not in stock.`)
+    return; 
+        }
+    }
 
-placeOrder("Lolipop");
+    for (let item of orderedItems) {
+        const inventoryItem = inventory.find(element => element.name === item.name);
+        inventoryItem.quantity -= item.quantity; 
+    }
+    orders.push({
+        customerName: customerName,
+        items: orderedItems,
+        status: 'Pending',
+    });
+
